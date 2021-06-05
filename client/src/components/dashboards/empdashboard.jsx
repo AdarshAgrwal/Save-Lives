c7667225b3b8b336cd1f544db8bafe6032edee5d
Arsh <Codetheon@users.noreply.github.com>
2021-06-05 19:16:19 +0530

    Bug Sorted

--- client/src/components/dashboards/empdashboard.jsx
+++ client/src/components/dashboards/empdashboard.jsx
@@ -2,57 +2,56 @@ import {useEffect} from 'react'
 import {useHistory} from 'react-router-dom'
 
 const Empdashboard = () => {
-    const history = useHistory()
+    // const history = useHistory()
 
-    const callExecution = async ()=>{
-        try{
-            //Sending the cookie back to the front end for futher processing
-            const res = await fetch('/empdashboard',{
-                method : "GET",
-                headers : {
-                    "Content-Type":"application/json",
-                    Accept : "application/json"
-                },
-                credentials:"include"
-            })
+    // const callExecution = async ()=>{
+    //     try{
+    //         //Sending the cookie back to the front end for futher processing
+    //         const res = await fetch('/empdashboard',{
+    //             method : "GET",
+    //             headers : {
+    //                 "Content-Type":"application/json",
+    //                 Accept : "application/json"
+    //             },
+    //             credentials:"include"
+    //         })
 
-            const data = await res.json()
+    //         const data = await res.json()
 
-            console.log(data)
+    //         console.log(data)
 
-            if (res.status !== 200){
-                throw new Error (res.error)
-            }else{
-                console.log("We got the Data successfully from the cookie")
-            }
-        }catch(err){
-            console.log(err)
-            history.push('/employeelogin')
-        }
+    //         if (res.status !== 200){
+    //             throw new Error (res.error)
+    //         }else{
+    //             console.log("We got the Data successfully from the cookie")
+    //         }
+    //     }catch(err){
+    //         console.log(err)
+    //         history.push('/employeelogin')
+    //     }
+    // }
 
-    }
-
-    useEffect(()=>{
-        callExecution()
-    })
+    // useEffect(()=>{
+    //     callExecution()
+    // })
 
     return (
         <>
         <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
             <li className="nav-item" role="presentation">
-                <button className="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
+                <button className="nav-link active" id="pills-home-tab" data-toggle="pill" data-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
             </li>
             <li className="nav-item" role="presentation">
-                <button className="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</button>
+                <button className="nav-link" id="pills-profile-tab" data-toggle="pill" data-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</button>
             </li>
             <li className="nav-item" role="presentation">
-                <button className="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</button>
+                <button className="nav-link" id="pills-contact-tab" data-toggle="pill" data-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</button>
             </li>
             </ul>
             <div className="tab-content" id="pills-tabContent">
-            <div className="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">...</div>
-            <div className="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">...</div>
-            <div className="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">...</div>
+            <div className="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">home</div>
+            <div className="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">profile</div>
+            <div className="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">Contact</div>
             </div>
             
         </>
